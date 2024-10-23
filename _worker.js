61dfe18c06b1796936fdb3fefb6f0b914863a798
xy2yp <161705149+xy2yp@users.noreply.github.com>
2024-10-24 00:26:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1271,7 +1271,7 @@ async function proxyURL(proxyURL, url) {
 function checkSUB(host) {
 	if ((!sub || sub == '') && (addresses.length + addressesapi.length + addressesnotls.length + addressesnotlsapi.length + addressescsv.length) == 0){
 		addresses = [
-			'Join.my.Telegram.channel.CMLiussss.to.unlock.more.premium.nodes.cf.090227.xyz#加入我的频道t.me/CMLiussss解锁更多优选节点',
+			'Join.my.Telegram.channel.CMLiussss.to.unlock.more.premium.nodes.cf.090227.xyz#默认节点',
 			'127.0.0.1:1234#CFnat',
 			'visa.cn:443',
 			'singapore.com:8443',
